b50bcd368cf8f73175686224456121f4e483d253
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2023-05-30 17:10:00 +0530

    API fetched, pagination done

---
+++ src/Components/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// import React from 'react'
+import spinner from '../spinner.gif'
+
+
+export default function Spinner() {
+  return (
+    <div className='text-center'>   <img src={spinner} style={{ width: "5rem" }} alt="" /></div>
+
+  )
+}
--- src/Newsitem.js
+++ src/Newsitem.js
@@ -8,12 +8,12 @@ export default class Newsitem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imgurl}
+          <img src={imgurl ? imgurl:"https://akm-img-a-in.tosshub.com/indiatoday/images/breaking_news/202305/tialakvarma-sixteen_nine.jpg?VersionId=mLEnARZR0_GqzPaVcMvQXaEMk8JJOXZF"}
             className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
-            <a href={newsurl} target="_blank" className="btn btn-sm btn-primary">ReadMore</a>
+            <a  rel="noreferrer" href={newsurl} target="_blank" className="btn btn-sm btn-primary">ReadMore</a>
           </div>
         </div>
       </div>
